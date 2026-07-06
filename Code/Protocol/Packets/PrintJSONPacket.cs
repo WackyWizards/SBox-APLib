b08ff6adca95cc29a2da9562afa5b06a1e05c243
kellie <kellie@wackywizards.org>
2026-07-06 18:03:23 +0300

    Add missing properties to PrintJSONPart

--- Code/Protocol/Packets/PrintJSONPacket.cs
+++ Code/Protocol/Packets/PrintJSONPacket.cs
@@ -11,8 +11,28 @@ public sealed class PrintJSONPacket : APPacket
 	public PrintJSONPart[] Data { get; set; } = [];
 }
 
+#nullable enable
+
 public sealed class PrintJSONPart
 {
 	[JsonPropertyName( "text" )]
-	public string Text { get; set; }
+	public string Text { get; set; } = string.Empty;
+	
+	[JsonPropertyName( "type" )]
+	public string? Type { get; set; }
+	
+	[JsonPropertyName( "color" )]
+	public string? Color { get; set; }
+	
+	[JsonPropertyName( "flags" )]
+	public int? Flags { get; set; }
+	
+	[JsonPropertyName( "player" )]
+	public int? Player { get; set; }
+	
+	[JsonPropertyName( "item" )]
+	public long? Item { get; set; }
+	
+	[JsonPropertyName( "location" )]
+	public long? Location { get; set; }
 }
